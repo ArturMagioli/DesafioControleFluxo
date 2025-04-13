931681b0cbd435a3147459709dcc340f09b8929e
Artur Magioli <arturdepaula00@gmail.com>
2025-04-13 16:23:30 -0300

    feat: implementação do código

--- src/Contador.java
+++ src/Contador.java
@@ -6,19 +6,27 @@ public class Contador {
     public static void main(String[] args) throws Exception {
         Scanner terminal = new Scanner(System.in);
 
+        System.out.print("Primeiro número: ");
         int parametroUm = terminal.nextInt();
+
+        System.out.print("Segundo número: ");
         int parametroDois = terminal.nextInt();
 
         try {
             contar(parametroUm, parametroDois);
         }catch (ParametrosValidosException e) {
-
+            System.out.println("Error: O primeiro número é maior que o segundo!");
         }
 
         terminal.close();
     }
 
     static void contar(int parametroUm, int parametroDois) throws ParametrosValidosException {
+        if (parametroUm > parametroDois)
+            throw new ParametrosValidosException();
         
+        for (int i = parametroUm; i <= parametroDois; i++) {
+            System.out.println("Imprimindo o número " + i);
+        }
     }
 }
